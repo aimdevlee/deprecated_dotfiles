ce372fbb9f72ec2c186a6b9566e217a3649f4622
Dongbin Lee <dongbin-lee@c-fo.com>
2024-10-29 15:14:48 +0900

    feat: update noice config

--- dot_config/nvim/lua/plugins/utility.lua
+++ dot_config/nvim/lua/plugins/utility.lua
@@ -255,11 +255,8 @@ return {
             view = "mini",
           },
         },
-        presets = {
-          bottom_search = false,
-          command_palette = true,
-          long_message_to_split = true,
-          lsp_doc_border = true,
+        cmdline = {
+          view = "cmdline",
         },
       }
 
